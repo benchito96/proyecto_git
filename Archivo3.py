22dd193723dbac099bc66d92883c20d5fc26b731
Benchito <bvalenciasantacruz96@gmail.com>
2025-04-04 14:58:05 -0500

    Correcion conflicto, Rama main agrego linea de codigo al Archivo3

--- Archivo3.py
+++ Archivo3.py
@@ -1,2 +1,2 @@
 print("Hello trillizo")
-print("Este codigo esta puesto por los bandidos de la rama main (pero ahora yo un chamito de la rama1 te modifique el codigo)")
+print("Este codigo esta puesto por los bandidos de la rama main (yo de main agrego esto)")
