83331e3df5291b7f05edb132376be8a08e3ad5b1
Benchito <bvalenciasantacruz96@gmail.com>
2025-04-04 14:09:14 -0500

    Rama rama1 modifico linea de codigo del Archivo3, cuando era trabajo de la rama main

--- Archivo3.py
+++ Archivo3.py
@@ -1,2 +1,2 @@
 print("Hello trillizo")
-print("Este codigo esta puesto por los bandidos de la rama main")
+print("Este codigo esta puesto por los bandidos de la rama main (pero ahora yo un chamito de la rama1 te modifique el codigo)")
